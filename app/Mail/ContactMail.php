58b7c4b7273eace2646b7568cda0fb1d89525cdc
Amanuel Desalegn <amandesalegnb@gmail.com>
2023-04-28 16:09:41 +0300

    fix blade

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -13,26 +13,41 @@ class ContactMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $data;
-
     /**
      * Create a new message instance.
-     *
-     * @return void
      */
-    public function __construct($data)
+    public function __construct()
     {
-        $this->data = $data;
+        //
     }
 
     /**
-     * Build the message.
-     *
-     * @return $this
+     * Get the message envelope.
      */
-    public function build()
+    public function envelope(): Envelope
     {
-        return $this->subject('Contact US - Ednadent')
-            ->view('emails.contact');
+        return new Envelope(
+            subject: 'Contact Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.contact',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
     }
 }

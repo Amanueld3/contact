dcff250531cd9055964ab7cddca627ed62fde98d
Amanuel Desalegn <amandesalegnb@gmail.com>
2023-04-28 15:54:58 +0300

    fix blade

---
+++ resources/emails/contact.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Us</title>
+</head>
+
+<body>
+    <h2>Hey, It's me {{ $data->name }}</h2>
+    <br>
+
+    <strong>User details: </strong><br>
+    <strong>Name: </strong>{{ $data->name }} <br>
+    <strong>Email: </strong>{{ $data->email }} <br>
+    <strong>Message: </strong>{{ $data->message }} <br><br>
+
+    Thank you
+
+</body>/upload/laravel-contact-us-form-2.png
+
+</html>
